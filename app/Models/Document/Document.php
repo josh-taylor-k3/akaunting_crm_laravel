akaunting/akaunting

use the correct translation for cancellations
* Some languages have different words for cancelling an action vs
  cancelling an invoice.

app/Models/Document/Document.php
@@ -614,7 +614,7 @@ class Document extends Model
             if ($this->status != 'cancelled') {
                 try {
                     $actions[] = [
-                        'title' => trans('general.cancel'),
+                        'title' => trans('documents.actions.mark_cancelled'),
                         'icon' => 'cancel',
                         'url' => route($prefix . '.cancelled', $this->id),
                         'permission' => 'update-' . $group . '-' . $permission_prefix,

resources/views/components/documents/show/more-buttons.blade.php
@@ -114,7 +114,7 @@
 
                 @if ($document->status != 'cancelled')
                     <x-dropdown.link href="{{ route($cancelledRoute, $document->id) }}" id="show-more-actions-cancel-{{ $document->type }}">
-                        {{ trans('general.cancel') }}
+                        {{ trans('documents.actions.mark_cancelled') }}
                     </x-dropdown.link>
                 @endif
             @endcan

josh-taylor-k3/akaunting_crm_laravel

app/Models/Document/Document.php
@@ -614,7 +614,7 @@ class Document extends Model
             if (! in_array($this->status, ['cancelled', 'draft'])) {
                 try {
                     $actions[] = [
-                        'title' => trans('general.cancel'),
+                        'title' => trans('documents.actions.cancel'),
                         'icon' => 'cancel',
                         'url' => route($prefix . '.cancelled', $this->id),
                         'permission' => 'update-' . $group . '-' . $permission_prefix,

resources/views/components/documents/show/more-buttons.blade.php
@@ -113,7 +113,7 @@
                 <x-dropdown.divider />
 
                 <x-dropdown.link href="{{ route($cancelledRoute, $document->id) }}" id="show-more-actions-cancel-{{ $document->type }}">
-                    {{ trans('general.cancel') }}
+                    {{ trans('documents.actions.cancel') }}
                 </x-dropdown.link>
             @endcan
         @endif